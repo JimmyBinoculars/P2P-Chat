58013eb32d209e63779b82d7d47c6f17be92f346
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2023-06-11 08:00:03 +0100

    Added key generator

--- server.js
+++ server.js
@@ -1,26 +1,58 @@
 const net = require('net');
 const port = 5555;
+const crypto = require('crypto');
+let currentKey;
+
+function generateRandomString(lengthInBits) {
+  const lengthInBytes = Math.ceil(lengthInBits / 8);
+  const randomBytes = crypto.randomBytes(lengthInBytes);
+  const randomString = randomBytes.toString('hex');
+  return randomString;
+}
+//string check
+const randomString = generateRandomString(256);
+console.log(randomString);
 
 const server = net.createServer((socket) => {
     console.log('Client connected.');
+  
+    // Add the socket to the list of connected sockets
+    const clients = [];
+    clients.push(socket);
   
     // Handle incoming data from the client
     socket.on('data', (data) => {
       const message = data.toString().trim();
       console.log(`Received message: ${message}`);
-  
-      // Send a response back to the client
-      socket.write(`Server received message: ${message}\n`);
     });
   
     // Handle client disconnection
     socket.on('end', () => {
       console.log('Client disconnected.');
+  
+      // Remove the socket from the list of connected sockets
+      const index = clients.indexOf(socket);
+      if (index !== -1) {
+        clients.splice(index, 1);
+      }
     });
   });
-
 server.listen(port);
 
-while (true()){
-
+async function mainLoop(){
+    while (true()){
+        currentKey = generateRandomString(256);
+        console.log(`Current key is ${currentKey}`)
+        await sleep(1800000);
+    }
 }
+async function sendData(){
+    while (true()){
+        clients.forEach((client) => {
+            client.write(currentKey);
+        });
+    }
+}
+
+mainLoop();
+sendData()
