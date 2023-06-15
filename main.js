ff1fac3221b9bef5e6dce9944491a9793fd80c2e
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2023-06-15 20:39:49 +0100

    stuff

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
 const net = require('node:net');
 const fs = require('node:fs');
 const path = require('node:path');
-const readlineSync = require('readline-sync');
 var CryptoJS = require("crypto-js");
 const client = new net.Socket();
-const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 let identifierBase, key;
 let currentKey;
 
@@ -87,7 +81,7 @@ function start(){
       client.on('data', (data) => {
         const receivedData = data.toString().trim();
         currentKey = receivedData;
-        //console.log(`Received data: ${receivedData}`);
+        console.log(`Received data: ${receivedData}`);
       });
     
       client.on('close', () => {
